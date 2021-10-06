1d047f251cdf929757444e29264f79393b7a533f
Luiz Fernando Alvino <luiz_alvino@hotmail.com>
2021-10-06 16:43:53 -0400

    feature: PWA working

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,13 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div key={id} className="bg-light-green dib br3 pa3 ma2 grow shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+      <img
+        alt="robots"
+        src={`https://robohash.org/${id}?size=200x200`}
+        srcSet={`https://robohash.org/${id}?size=200x200 200w, https://robohash.org/${id}?size=400x400 1080w`}
+        width="200"
+        height="200"
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
